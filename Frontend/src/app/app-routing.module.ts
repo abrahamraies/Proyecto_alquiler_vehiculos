484efde20bbabbf283f51974d1bafabc49e8e1f9
abrahamraies <abrahamraies@gmail.com>
2022-11-04 20:30:44 -0300

    Cambio nombres carpetas

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import { ModificarVehiculoComponent } from './components/modificar-vehiculo/modificar-vehiculo.component';
-import { RegistrarAlquilerVehiculoComponent } from './components/registrar-alquiler-vehiculo/registrar-alquiler-vehiculo.component';
+import { ModificarVehiculoComponent } from './components/vehiculo-modificar/modificar-vehiculo.component';
+import { RegistrarAlquilerVehiculoComponent } from './components/registro-alquiler-vehiculo/registrar-alquiler-vehiculo.component';
 import { HomeComponent } from './components/home/home.component';
 import { ListaVehiculosComponent } from './components/lista-vehiculos/lista-vehiculos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
-import { RegitroEmpresaComponent } from './components/regitro-empresa/regitro-empresa.component';
+import { RegitroEmpresaComponent } from './components/registro-empresa/regitro-empresa.component';
 import { LoginEmpresaComponent } from './components/login-empresa/login-empresa.component';
 import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
 import { EmpresaModificarComponent } from './components/empresa-modificar/empresa-modificar.component';
